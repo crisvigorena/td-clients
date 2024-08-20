f3e798ffe2d01e5977bdd91a381579bc408db7ed
Rigo <rodrigo@tiktaalik.dev>
2024-08-20 07:35:59 -0400

    Implemented all methods in  class ClienteServicio.java. ToDo: Add comments and document methods.

--- src/servicio/ClienteServicio.java
+++ src/servicio/ClienteServicio.java
@@ -5,6 +5,7 @@ import modelo.Cliente;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ClienteServicio {
 
@@ -19,7 +20,9 @@ public class ClienteServicio {
 
     // Crear un método para listar todos los clientes
     public void listarClientes() {
-
+        for (Cliente record: listaClientes) {
+            System.out.println(record.toString());
+        }
     }
 
     // Crear un método para agregar nuevos clientes
@@ -27,13 +30,22 @@ public class ClienteServicio {
                    @SuppressWarnings("SpellCheckingInspection") String aniosCliente,
                    @SuppressWarnings("SpellCheckingInspection") CategoriaEnum nombreCategoria) {
 
+        // Crear un objeto Cliente y agregarlo a la lista de Clientes
+        listaClientes.add(new Cliente(runCliente, nombreCliente, apellidoCliente, aniosCliente, nombreCategoria));
     }
 
     // Crear un método para editar los datos de un cliente
     public void editarCliente(String runCliente, String nombreCliente, String apellidoCliente,
                    @SuppressWarnings("SpellCheckingInspection") String aniosCliente,
                    @SuppressWarnings("SpellCheckingInspection") CategoriaEnum nombreCategoria) {
-
+        for (Cliente record: listaClientes) {
+            if (Objects.equals(record.getRunCliente(), runCliente)) {
+                record.setNombreCliente(nombreCliente);
+                record.setApellidoCliente(apellidoCliente);
+                record.setAniosCliente(aniosCliente);
+                record.setNombreCategoria(nombreCategoria);
+            }
+        }
     }
 
     // Generar un accesador
